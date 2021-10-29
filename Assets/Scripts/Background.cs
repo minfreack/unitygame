75fc3116c8305add7b9c001a3ef27f19f4315418
Luis Ramirez <luis@lapieza.io>
2021-10-29 17:09:44 -0500

    First commit

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    float scrollSpeed = 0.2f;
+    Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Boundary{
+	public float xMinimum, xMaximum, yMinimum, yMaximum;
+}
+
+public class PlayerController : MonoBehaviour
+{		
+		public Move moverComponent;
+		public float speed;
+		public Boundary boundary;
+
+		public Transform shootOrigin;
+		public GameObject shootPrefab;
+    private void Start()
+    {
+        moverComponent.speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {		
+			Vector3 direction = new Vector3(Input.GetAxis("Horizontal"),
+				Input.GetAxis("Vertical"),
+				transform.position.z);
+			moverComponent.direction = direction;
+
+			float x = Mathf.Clamp(transform.position.x, boundary.xMinimum, boundary.xMaximum);
+			float y = Mathf.Clamp(transform.position.y, boundary.yMinimum, boundary.yMaximum);
+			transform.position = new Vector3(x, y);
+
+			if (Input.GetButtonDown("Shoot")){
+				Instantiate(shootPrefab, shootOrigin, false);
+			}
+		}
+}
